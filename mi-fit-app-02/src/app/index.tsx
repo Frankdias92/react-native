a8fd6a0217e8d677ac1ffbc7e6445e2a90f3d98c
Frankdias92 <frankmcdias@gmail.com>
2024-08-31 18:08:50 -0300

    :sparkles: Feat: base config settings

--- mi-fit-app-02/src/app/index.tsx
+++ mi-fit-app-02/src/app/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
+import './global.css'
 
 export default function App() {
     return (
-        <View>
-            <Text>Test project</Text>
+        <View className='flex flex-1 bg-black'>
+            <Text className='text-4xl text-white'>Test project</Text>
         </View>
     )
 }
