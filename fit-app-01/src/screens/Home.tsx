7c2b1cfc0e6061f98e113869f081a75a580a6221
Frankdias92 <frankmcdias@gmail.com>
2024-09-02 22:17:33 -0300

    :sparkles: Feat: add component group for list exercice

--- fit-app-01/src/screens/Home.tsx
+++ fit-app-01/src/screens/Home.tsx
@@ -1,11 +1,33 @@
+import { Group } from "@components/home/Group";
 import { HomeHeader } from "@components/home/HomeHeader";
-import { VStack } from "@gluestack-ui/themed";
+import { HStack, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 
 export function Home() {
+    const [groups, setGroups] = useState(['Back', 'Legs', 'Arms', 'Biceps'])
+    const [groupSelected, setGroupSelected] = useState('biceps')
+    
     return (
         <VStack>
             <HomeHeader />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 32}}
+                style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+            />
+            
         </VStack>
     )
 }
