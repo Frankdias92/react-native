892da61aacdfb67bc458c7d06cb778f2f91b6135
Frankdias92 <frankmcdias@gmail.com>
2024-10-24 20:04:19 -0300

    :sparkles: Feat: setting NativeWind

--- react-ative-fundamentals/settingGluestack-v2/app/index.tsx
+++ react-ative-fundamentals/settingGluestack-v2/app/index.tsx
@@ -4,7 +4,7 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 export default function Index() {
   return (
     <View style={styles.container}>
-     <Text>React native</Text>
+     <Text className='font-bold text-2xl'>Setting NativeWind</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       {/* <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} /> */}
