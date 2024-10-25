3c574baa93549947669cbe3a24ad227d84224afa
Frankdias92 <frankmcdias@gmail.com>
2024-10-25 10:44:21 -0300

    :sparkles: Feat: add hand gesture and drawer

---
+++ react-ative-fundamentals/reactNative-v0-76/src/app/(drawer)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function Home() {
+  return (
+    <View className="flex-1 justify-center items-center bg-cyan-950">
+      <Text className="color-gray-100 text-2xl font-semibold">Page Home</Text>
+    </View>
+  )
+}
--- react-ative-fundamentals/reactNative-v0-76/src/app/_layout.tsx
+++ react-ative-fundamentals/reactNative-v0-76/src/app/_layout.tsx
@@ -1,9 +1,31 @@
-import { Slot } from "expo-router";
+// import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { Drawer } from 'expo-router/drawer'
+import { MaterialIcons } from '@expo/vector-icons'
+
+const Drawer = createDrawerNavigator() 
 
 import '../../global.css'
+import { View } from 'react-native'
+import Home from './(drawer)';
 
 export default function RootLayout() {
   return (
-    <Slot />
+    <Drawer.Navigator screenOptions={{headerShown: false}}>
+        <Drawer.Screen 
+          name="(drawer)/index"          
+          component={Home}
+          options={{
+            drawerLabel: 'Home',
+            drawerIcon: ({ color, size }) => (
+              <MaterialIcons 
+                name='home'
+                size={size}
+                color={color}
+              />
+            )
+          }}
+        />
+    </Drawer.Navigator>
   )
 }
