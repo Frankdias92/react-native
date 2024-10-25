f9b6f6c07a7972f52e7bfa2a234ed4868d28b44b
Frankdias92 <frankmcdias@gmail.com>
2024-10-25 09:27:02 -0300

    :tada: setting reactNative-v0.76 with NativeWind

---
+++ react-ative-fundamentals/reactNative-v0-76/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-cyan-950">
+      <Text className="color-gray-100 text-2xl font-semibold">React Native v0.76</Text>
+    </View>
+  )
+}
